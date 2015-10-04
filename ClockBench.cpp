0ed68b7d5a7b4d63f30a3a44700cd0c8464ed88c
Bill Torpey <wtorpey@gmail.com>
2015-10-04 14:05:45 -0400

    update ClockBench.cpp
    - add copyright notice
    - fix off-by-one error pointed out by Wolfgang
    - simplify stats printing
    - add param to get detail output to a file

--- ClockBench.cpp
+++ ClockBench.cpp
@@ -1,3 +1,7 @@
+// Copyright 2014 by Bill Torpey. All Rights Reserved.
+// This work is licensed under a Creative Commons Attribution-NonCommercial-NoDerivs 3.0 United States License.
+// http://creativecommons.org/licenses/by-nc-nd/3.0/us/deed.en
+
 #include <time.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -18,7 +22,7 @@ inline unsigned long long cpuid_rdtsc() {
   unsigned int lo, hi;
   asm volatile (
      "cpuid \n"
-     "rdtsc" 
+     "rdtsc"
    : "=a"(lo), "=d"(hi) /* outputs */
    : "a"(0)             /* inputs */
    : "%ebx", "%ecx");     /* clobbers*/
@@ -28,7 +32,7 @@ inline unsigned long long cpuid_rdtsc() {
 inline unsigned long long rdtsc() {
   unsigned int lo, hi;
   asm volatile (
-     "rdtsc" 
+     "rdtsc"
    : "=a"(lo), "=d"(hi) /* outputs */
    : "a"(0)             /* inputs */
    : "%ebx", "%ecx");     /* clobbers*/
@@ -38,7 +42,7 @@ inline unsigned long long rdtsc() {
 inline unsigned long long rdtscp() {
   unsigned int lo, hi;
   asm volatile (
-     "rdtscp" 
+     "rdtscp"
    : "=a"(lo), "=d"(hi) /* outputs */
    : "a"(0)             /* inputs */
    : "%ebx", "%ecx");     /* clobbers*/
@@ -54,8 +58,7 @@ do { \
       int n = i % (BUCKETS); \
       timestamp[n] = (x.tv_sec * ONE_BILLION) + x.tv_nsec; \
    } \
-   deltaT x(timestamp); \
-   printf("%25s\t", #CLOCK); \
+   deltaT x(#CLOCK, timestamp); \
    x.print(); \
 } while(0)
 
@@ -63,49 +66,47 @@ do { \
 
 struct deltaT   {
 
-   deltaT(vector<long> v) : sum(0), sum2(0), min(-1), max(0), avg(0), median(0), stdev(0) 
+   deltaT(const char* name, vector<long> v) : sum(0), sum2(0), min(-1), max(0), avg(0), median(0), stdev(0), name(name)
    {
       // create vector with deltas between adjacent entries
       x = v;
-      for (int i = 0; i < x.size(); ++i) {
-         x[i] = x[i+1] - x[i];
-      }      
       count = x.size() -1;
-      
+      for (int i = 0; i < count; ++i) {
+         x[i] = x[i+1] - x[i];
+      }
+
       for (int i = 0; i < count; ++i) {
          sum    += x[i];
          sum2   += (x[i] * x[i]);
          if (x[i] > max) max = x[i];
          if ((min == -1) || (x[i] < min)) min = x[i];
       }
-      
+
       avg = sum / count;
       median = min + ((max - min) / 2);
       stdev =  sqrt((count  * sum2 - (sum * sum)) / (count * count));
    }
-   
+
    void print()
    {
+      printf("%25s\t", name);
       printf("%ld\t%7.2f\t%7.2f\t%7.2f\t%7.2f\t%7.2f\n", count, min, max, avg, median, stdev);
-   } 
-   
-   void dump()
+   }
+
+   void dump(FILE* file)
    {
-      printf("%7.2f\t%7.2f\n", sum, sum2);
-      printf("\n");
+      if (file == NULL)
+         return;
+
+      fprintf(file, "%s", name);
       for (int i = 0 ; i < count; ++i ) {
-         if ((i % 8) == 0)
-            printf("\n");
-         else
-            printf("\t");
-         printf("[%03d]=%ld", i, x[i]);
+         fprintf(file, "\t%ld", x[i]);
       }
-      
-      printf("\n");
-   } 
+      fprintf(file, "\n");
+   }
 
    vector<long> x;
-   long   count;   
+   long   count;
    double sum;
    double sum2;
    double min;
@@ -113,7 +114,9 @@ struct deltaT   {
    double avg;
    double median;
    double stdev;
+   const char* name;
 };
+
 
 
 
@@ -123,14 +126,20 @@ int main(int argc, char** argv)
    if (argc > 1) {
       CPU_FREQ = strtod(argv[1], NULL);
    }
-   
+
+   FILE* file = NULL;
+   if (argc > 2) {
+      file = fopen(argv[2], "w");
+   }
+
+
    vector<long>    timestamp;
    timestamp.resize(BUCKETS);
-   
+
    printf("%25s\t", "Method");
    printf("%s\t%7s\t%7s\t%7s\t%7s\t%7s\n", "samples", "min", "max", "avg", "median", "stdev");
-   
-#if _POSIX_TIMERS > 0   
+
+#if _POSIX_TIMERS > 0
    #ifdef CLOCK_REALTIME
    do_clock(CLOCK_REALTIME);
    #endif
@@ -158,51 +167,48 @@ int main(int argc, char** argv)
 
 
    {
-      for (int i = 0; i < ITERS * BUCKETS; ++i) { 
-         int n = i & 0xff; 
+      for (int i = 0; i < ITERS * BUCKETS; ++i) {
+         int n = i & 0xff;
          timestamp[n] = cpuid_rdtsc();
       }
-      for (int i = 0; i < BUCKETS; ++i) { 
+      for (int i = 0; i < BUCKETS; ++i) {
          timestamp[i] = (long) ((double) timestamp[i] / CPU_FREQ);
       }
-      deltaT x(timestamp); 
-      printf("%25s\t", "cpuid+rdtsc");
-      x.print(); 
-      //x.dump();
+      deltaT x("cpuid+rdtsc", timestamp);
+      x.print();
+      x.dump(file);
    }
 
    // will throw SIGILL on machine w/o rdtscp instruction
    #ifdef RDTSCP
    {
-      for (int i = 0; i < ITERS * BUCKETS; ++i) { 
-         int n = i & 0xff; 
+      for (int i = 0; i < ITERS * BUCKETS; ++i) {
+         int n = i & 0xff;
          timestamp[n] = rdtscp();
       }
-      for (int i = 0; i < BUCKETS; ++i) { 
+      for (int i = 0; i < BUCKETS; ++i) {
          timestamp[i] = (long) ((double) timestamp[i] / CPU_FREQ);
       }
-      deltaT x(timestamp); 
-      printf("%25s\t", "rdtscp");
-      x.print(); 
-      //x.dump();
+      deltaT x("rdtscp", timestamp);
+      x.print();
+      x.dump(file);
    }
    #endif
-   
+
    {
-      for (int i = 0; i < ITERS * BUCKETS; ++i) { 
-         int n = i & 0xff; 
+      for (int i = 0; i < ITERS * BUCKETS; ++i) {
+         int n = i & 0xff;
          timestamp[n] = rdtsc();
       }
-      for (int i = 0; i < BUCKETS; ++i) { 
+      for (int i = 0; i < BUCKETS; ++i) {
          timestamp[i] = (long) ((double) timestamp[i] / CPU_FREQ);
       }
-      deltaT x(timestamp); 
-      printf("%25s\t", "rdtsc");
-      x.print(); 
-      //x.dump();
+      deltaT x("rdtsc", timestamp);
+      x.print();
+      x.dump(file);
    }
 
    printf("Using CPU frequency = %f\n", CPU_FREQ);
 
-   return 0;   
+   return 0;
 }
